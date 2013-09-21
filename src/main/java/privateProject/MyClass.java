d9891219ddbe48e4ac88003823a1c7e298e8ef4c
genmymodel <help@genmymodel.com>
2013-09-21 15:20:58 +0200

    commit by genmymodel from www.genmymodel.com: v20130921_152057

---
+++ src/main/java/privateProject/MyClass.java
@@ -0,0 +1,31 @@
+package privateProject;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class MyClass
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String attribute;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public MyClass(){
+		super();
+	}
+
+}
+
